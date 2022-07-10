0708c727a3ec03584ef22f1100d49b7ab669319a
fast08960 <fast089609@gmail.com>
2022-07-09 21:48:25 -0500

    fotn edede

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-        <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <a className="h-10 px-5 m-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg hover:bg-indigo-800 h-8 px-4 text-sm inline-flex items-center justify-center">Small</a>
+      <Navbar></Navbar>
     </div>
   )
 }
---
+++ src/fontawesome.js
@@ -0,0 +1,13 @@
+// import the library
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// import your icons
+import { faCode, faHighlighter } from '@fortawesome/free-solid-svg-icons';
+import { faCode, faHighlighter } from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  faMoneyBill,
+  faCode,
+  faHighlighter
+  // more icons go here
+);
