ee8eb8511caba73083c6879e058e5703e0c26aa1
fast08960 <fast089609@gmail.com>
2022-07-11 16:10:53 -0500

    change

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Navbar from './components/Navbar'
+import Slider from './components/Slider'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Slider></Slider>
     </div>
   )
 }
